d4f14108a53a3c0170c577b6bc76d8974ea84e0c
jhermann <jh@web.de>
2015-03-24 05:25:46 +0100

    :zzz: future import

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -23,6 +23,8 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 """
+from __future__ import absolute_import, unicode_literals, print_function
+
 import os
 import sys
 import pprint
